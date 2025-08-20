cad4f0e222d4cac99a02dfdbcf91caec19b82aad
Kirushka <kirillzaporozec48@gmail.com>
2025-08-20 16:46:44 +0700

    create game.go

---
+++ game/game.go
@@ -0,0 +1,67 @@
+package game
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font/basicfont"
+)
+
+type Game struct{
+	leftPaddle *Paddle 
+	rightPaddle *Paddle 
+	ball *Ball 
+	score *Score 
+	paused bool
+}
+
+func NewGame() *Game{
+	return &Game{
+		leftPaddle: NewPaddle(true),
+		rightPaddle: NewPaddle(false),
+		ball: NewBall(),
+		score: NewScore(),
+		paused: false,
+	}
+}
+
+func (g *Game) Update() error{
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace){
+		g.paused = !g.paused
+	}
+
+	if g.paused{
+		return nil
+	}
+	g.leftPaddle.Update()
+	g.rightPaddle.Update()
+	g.ball.Update(g.leftPaddle, g.rightPaddle, g.score)
+
+	return nil
+
+}
+
+func (g *Game) Draw (screen *ebiten.Image) {
+	screen.Fill(color.White)
+
+	g.leftPaddle.Draw(screen)
+	g.rightPaddle.Draw(screen)
+	g.ball.Draw(screen)
+	g.score.Draw(screen)
+	
+	for y := 0; y < 600; y += 20 {
+		ebitenutil.DrawRect(screen, 400-1, float64(y), 2, 10, color.White)
+	}
+
+	if g.paused{
+		text.Draw(screen, "PAUSED", basicfont.Face7x13, 350, 300, color.Black)
+	}
+
+	func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int){
+		return 800, 600
+	}
+}
